fb1d49f13c28a55c85d9df382284c91c3dcc2ae9
Iegor Maiorov <maiorov.prog@gmail.com>
2017-12-31 11:46:34 +0200

    added bemchmark runner

---
+++ src/main/java/algorithms/BenchmarkRunner.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+import org.openjdk.jmh.runner.RunnerException;
+
+import java.io.IOException;
+
+public class BenchmarkRunner {
+
+    public static void main(String[] args) throws IOException, RunnerException {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
---
+++ src/test/java/algorithms/hackerrank/BigSortingTest.java
@@ -0,0 +1,12 @@
+package algorithms.hackerrank;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BigSortingTest {
+
+    @Test
+    public void name() {
+        BigSorting.bigSorting(new String[]{});
+    }
+}
